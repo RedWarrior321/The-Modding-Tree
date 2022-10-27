8f0649e499b5c59df78a6b178f5e4529dd7b16bf
RedWarrior321 <80589150+RedWarrior321@users.noreply.github.com>
2022-10-27 19:24:27 +0200

    Update mod.js
    Changed the name of the tree

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Grass Tree",
+	id: "01",
+	author: "Aspect",
+	pointsName: "Grass",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
